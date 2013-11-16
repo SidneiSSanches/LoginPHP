eadbc26ec67e996f2ee455042f6019b196ec1458
Sidnei S Sanches <sidneissanches@gmail.com>
2013-11-16 14:53:55 -0200

    Este commit é somente um teste

---
+++ linha/src/teste.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+import java.awt.event.*;
+public class teste extends Frame
+{  
+   public static void main(String[] args)
+   { new teste();
+   }
+   
+   teste()
+   {  super("Tela");
+      addWindowListener(new WindowAdapter()
+         {public void windowClosing(WindowEvent e){System.exit(0);}});
+      setSize(900, 500);
+      show();
+   }
+   public void paint(Graphics g)
+   {  
+  
+  
+      g.drawLine(60, 30, 10, 70);
+  g.setColor(Color.blue);
+      g.drawLine(10, 70, 10, 140);
+  g.setColor(Color.red);
+      g.drawLine(120, 140, 10, 140);
+  g.setColor(Color.green); 
+      g.drawLine(60, 30, 120, 70);
+  g.setColor(Color.orange);
+      g.drawLine(120, 70, 120, 140);
+   }
+
+   
+}
